72b7ed630fbcd1d4ec06648d4ec30a15cef46fc9
JosiasDEV <155330003+JosiasD3V@users.noreply.github.com>
2025-12-04 22:05:17 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJkrS5e52b1EO3yZIp0pv28J6VGyZX-L4",
+  apiKey: "*********???????????",
   authDomain: "tarefasplus-e15c8.firebaseapp.com",
   projectId: "tarefasplus-e15c8",
   storageBucket: "tarefasplus-e15c8.firebasestorage.app",
   messagingSenderId: "216023179141",
-  appId: "1:216023179141:web:dfe66de1fd0da465166eec"
+  appId: "1:216023179141:web:*******"
 };
 
 // Initialize Firebase
